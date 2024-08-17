f10dc98f96761ed54cd48728806894aaba8e27f3
Logan <monday.hopscotch@gmail.com>
2024-08-17 09:50:44 -0600

    basic reticle

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -1,5 +1,7 @@
 package states;
 
+import flixel.math.FlxPoint;
+import entities.Reticle;
 import entities.Item;
 import flixel.util.FlxColor;
 import debug.DebugLayers;
@@ -16,6 +18,7 @@ using states.FlxStateExt;
 
 class PlayState extends FlxTransitionableState {
 	var player:FlxSprite;
+	var reticle:FlxSprite;
 
 	override public function create() {
 		super.create();
@@ -32,20 +35,30 @@ class PlayState extends FlxTransitionableState {
 		player = new Player();
 		add(player);
 
-		var item = new Item();
-		item.y = 50;
-		add(item);
-
-		add(Achievements.ACHIEVEMENT_NAME_HERE.toToast(true, true));
+		// We want the reticle to likely live on the UI camera for ease of tracking the mouse?
+		// Or do we just want to project the mouse position into the game world cam?
+		reticle = new Reticle();
+		add(reticle);
+		
+		// add(Achievements.ACHIEVEMENT_NAME_HERE.toToast(true, true));
 
 		// QuickLog.error('Example error');
 	}
+	
+	var tmp = FlxPoint.get();
+	var tmp2 = FlxPoint.get();
 
 	override public function update(elapsed:Float) {
 		super.update(elapsed);
 
 		// var cam = FlxG.camera;
 		// DebugDraw.ME.drawCameraRect(cam.getCenterPoint().x - 5, cam.getCenterPoint().y - 5, 10, 10, DebugLayers.RAYCAST, FlxColor.RED);
+
+		reticle.getPosition(tmp);
+		player.getPosition(tmp2);
+		
+		tmp.addPoint(tmp2).scale(.5);
+		camera.focusOn(tmp);
 	}
 
 	override public function onFocusLost() {
